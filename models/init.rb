2d23e5721cffa624e57234860ed593684771cb0e
Louis Rawlins <louis.rawlins@gmail.com>
2012-03-16 11:06:01 -0700

    more vm reference removal

--- models/init.rb
+++ models/init.rb
@@ -4,7 +4,6 @@ require 'data_mapper'
 
 DataMapper.setup(:default, eval('"' + BasicUserSite.settings.storage_url + '"'))
 DataMapper.setup(:abstract, 'abstract::')
-DataMapper.setup(:vm_legacy, eval('"' + BasicUserSite.settings.vm_legacy_url + '"'))
 
 require_relative 'user'
 require_relative 'pwd_change_request'
